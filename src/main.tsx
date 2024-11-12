39e3a4ddcdd30deec33fd30f26b50139410b03be
wiktormalyska <wiktormalyska03@gmail.com>
2024-11-12 18:50:12 +0100

    Added root element background-color

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import HomePage from "./pages/HomePage.tsx";
+import colorPalette from "./values/colorPalette.tsx";
+
 
 const router = createBrowserRouter([
     {
@@ -13,6 +15,9 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+        <RouterProvider router={router}/>
   </StrictMode>,
 )
+
+const root = document.getElementById('root')!
+root.style.backgroundColor = colorPalette.base.hex
