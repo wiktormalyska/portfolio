f6275e69b81f300c4d31ddfc0ea05379174e348c
wiktormalyska <wiktormalyska03@gmail.com>
2024-11-12 18:28:14 +0100

    Added HomePage.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import HomePage from "./pages/HomePage.tsx";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div> Main </div>,
+        element: < HomePage/>,
     },
     ])
 
