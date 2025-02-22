dcab4e8f130a4548424a3f8c4740288dc3fd161c
Wiktor Małyska <wikimal200310@gmail.com>
2025-02-23 00:05:09 +0100

    fix projects width

--- frontend/src/pages/ProjectsPage.tsx
+++ frontend/src/pages/ProjectsPage.tsx
@@ -50,38 +50,36 @@ export function ProjectsPage() {
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{duration: 1}}
-            className="w-full flex justify-center pt-10 transition-opacity flex-col text-center">
-            {/*TODO: Uncomment when finished*/ }
-            {/*<span*/}
-            {/*    className="bg-gradient-to-r from-primary  to-accent inline-block text-transparent bg-clip-text text-6xl font-extrabold">*/}
-            {/*    PROJECTS*/}
-            {/*</span>*/}
-            {/*<div className="flex flex-row gap-10 pt-10">*/}
-            {/*    {mockedProjects.map(project => {*/}
-            {/*        return (*/}
-            {/*            <a key={project.gitHubRepositoryId} href={project.repositoryUrl} target="_blank" className="max-w-[50%]">*/}
-            {/*                <div className="relative flex flex-col w-full p-2 h-full">*/}
-            {/*                    <div*/}
-            {/*                        className="absolute inset-0  rounded-2xl backdrop-blur-4xl blur-xs bg-text opacity-6 h-[100%]"/>*/}
-            {/*                    <img className="w-full h-120 rounded-4xl object-cover" src={project.imageUrl}*/}
-            {/*                         alt={project.name}/>*/}
-            {/*                    <div*/}
-            {/*                        className="text-lg w-full flex flex-col justify-start text-start pl-5 pr-5 h-full">*/}
-            {/*                        <span className="pt-1  text-3xl font-bold tracking-wide">{project.name}</span>*/}
-            {/*                        <div className="flex w-full flex-col justify-between">*/}
-            {/*                            <div className="flex w-full flex-row justify-start gap-5">*/}
-            {/*                                <GetListOfIconsFromTechnologyList technologiesString={project.technologies}/>*/}
-            {/*                            </div>*/}
-            {/*                            <span className="pt-1">{project.description}</span>*/}
-            {/*                        </div>*/}
+            className="w-full flex justify-center pt-10 transition-opacity flex-col text-center pl-20 pr-20">
+            <span
+                className="bg-gradient-to-r from-primary  to-accent inline-block text-transparent bg-clip-text text-6xl font-extrabold">
+                PROJECTS
+            </span>
+            <div className="flex flex-row gap-10 pt-10">
+                {mockedProjects.map(project => {
+                    return (
+                        <a key={project.gitHubRepositoryId} href={project.repositoryUrl} target="_blank" className="w-[50%]">
+                            <div className="relative flex flex-col w-full p-2 h-full">
+                                <div
+                                    className="absolute inset-0  rounded-2xl backdrop-blur-4xl blur-xs bg-text opacity-6 h-[100%]"/>
+                                <img className="w-full h-120 rounded-4xl object-cover" src={project.imageUrl}
+                                     alt={project.name}/>
+                                <div
+                                    className="text-lg w-full flex flex-col justify-start text-start pl-5 pr-5 h-full">
+                                    <span className="pt-1  text-3xl font-bold tracking-wide">{project.name}</span>
+                                    <div className="flex w-full flex-col justify-between">
+                                        <div className="flex w-full flex-row justify-start gap-5">
+                                            <GetListOfIconsFromTechnologyList technologiesString={project.technologies}/>
+                                        </div>
+                                        <span className="pt-1">{project.description}</span>
+                                    </div>
 
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*            </a>*/}
-            {/*        )*/}
-            {/*    })}*/}
-            {/*</div>*/}
-
+                                </div>
+                            </div>
+                        </a>
+                    )
+                })}
+            </div>
         </motion.div>
     )
 }
