19582a815c54f1a86f8efdabf11ae171bc13fd49
wiktormalyska <wiktormalyska03@gmail.com>
2025-08-07 11:44:18 +0200

    feat: add country flag component to ResumePage and update layout

--- frontend/src/pages/ResumePage.tsx
+++ frontend/src/pages/ResumePage.tsx
@@ -1,4 +1,5 @@
 import {motion} from "motion/react";
+import CountryFlag from "ts-react-emoji-flag"
 
 export function ResumePage () {
     return (
@@ -6,8 +7,23 @@ export function ResumePage () {
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{duration: 1}}
-            className="w-full flex justify-center pt-20 transition-opacity">
-            <div className="text-6xl">This sub-page is still in progress...</div>
+            className="w-full flex justify-center pt-10 transition-opacity flex-col text-center pl-20 pr-20">
+            <span
+                className="bg-gradient-to-r from-primary  to-accent inline-block text-transparent bg-clip-text text-5xl font-extrabold">
+                RESUME
+            </span>
+            <div
+                className="flex flex-row w-full mt-5 justify-center gap-5 text-center items-center text-xl">
+                <div
+                    className="absolute opacity-6 rounded-2xl backdrop-blur-4xl blur-xs bg-text w-[50%] h-15" />
+                <div
+                    className="bg-neutral-900/30 p-2 rounded-2xl" >
+                    <CountryFlag countryCode={"PL"} />
+                </div>
+                <div>
+                    PL
+                </div>
+            </div>
 
         </motion.div>
     )
