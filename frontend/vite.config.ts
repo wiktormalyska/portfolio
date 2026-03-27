d0448a91e273a7686a331e5b4a608a771c3e76a0
wiktormalyska <wiktormalyska03@gmail.com>
2026-03-27 10:49:22 +0100

    feat: add robots.txt and integrate sitemap generation with vite-plugin-sitemap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,23 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
+        Sitemap({
+            hostname: 'https://wiktormalyska.ovh',
+            dynamicRoutes: [
+                '/projects',
+                '/resume',
+                '/contact',
+                '/Wiktor_Malyska_PL.pdf',
+                '/Wiktor_Malyska_ENG.pdf'
+            ]
+        })
     ],
     server: {
         port: 3000,
